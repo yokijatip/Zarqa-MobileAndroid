068f68a94866ece4e0ab239b059370943bfdcaad
Yoki Jati Perkasa <yokijati@gmail.com>
2025-01-16 07:22:28 +0700

    Success Implement Auto Login in Splash Screeen

--- app/src/main/java/com/gity/myzarqa/presentation/common/splash/SplashActivity.kt
+++ app/src/main/java/com/gity/myzarqa/presentation/common/splash/SplashActivity.kt
@@ -3,13 +3,10 @@ package com.gity.myzarqa.presentation.common.splash
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.WindowManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
-import com.gity.myzarqa.R
 import com.gity.myzarqa.databinding.ActivitySplashBinding
 import com.gity.myzarqa.presentation.auth.AuthActivity
 import com.gity.myzarqa.presentation.common.base.MainActivity
@@ -18,6 +15,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import timber.log.Timber
 
+@Suppress("DEPRECATION")
 @AndroidEntryPoint
 @SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
@@ -29,7 +27,8 @@ class SplashActivity : AppCompatActivity() {
         binding = ActivitySplashBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        setupUI()
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
         // Gunakan Timber untuk logging
         Timber.d("SplashActivity Created")
         checkLoginStatus()
@@ -37,8 +36,8 @@ class SplashActivity : AppCompatActivity() {
 
     private fun checkLoginStatus() {
         lifecycleScope.launch {
-            // Delay 2 detik untuk menampilkan splash screen
-            delay(2000)
+            // Delay 3 detik untuk menampilkan splash screen
+            delay(3000)
 
             if (viewModel.checkLoginStatus()) {
                 Timber.d("User is logged in, navigating to MainActivity")
@@ -48,15 +47,6 @@ class SplashActivity : AppCompatActivity() {
                 startActivity(Intent(this@SplashActivity, AuthActivity::class.java))
             }
             finish()
-        }
-    }
-
-    private fun setupUI(){
-        enableEdgeToEdge()
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
 }
